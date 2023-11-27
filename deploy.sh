4164cd4f7541eaf55f9b56d484f39b2f99e366c4
jurjenwerkaccount <jurjen.v.genugten@gmail.com>
2023-11-27 16:22:03 +0100

    Change to start stop method of running

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Check if all required arguments are provided
 if [ "$#" -ne 7 ]; then
-  echo "Usage: $0 <pipeline_name> <github_token> <docker_username> <docker_password> <azure_client_id> <azure_client_secret> <azure_tenant_id>"
+  echo "Usage: $0 <pipeline_name> <github_token> <docker_username> <docker_password>"
   exit 1
 fi
 
@@ -10,9 +10,6 @@ PIPELINE_NAME="$1"
 GITHUB_TOKEN="$2"
 DOCKER_USERNAME="$3"
 DOCKER_PASSWORD="$4"
-AZURE_CLIENT_ID="$5"
-AZURE_CLIENT_SECRET="$6"
-AZURE_TENANT_ID="$7"
 
 # Clone the GitHub repository
 echo "Cloning repository: bureaugewas/vgde_datasets_pipeline"
@@ -42,7 +39,7 @@ echo "Step 2: Pushing Docker image to Docker Hub"
 docker tag vgdedatasets bureaugewas/vgdedatasets:latest
 docker push bureaugewas/vgdedatasets:latest
 
-# Step 3: Create Docker container on Raspberry Pi
+# Step 3: Create Docker container on Raspberry Pi (azure vars should be set in the machine's environment)
 echo "Step 3: Creating Docker container on Raspberry Pi"
 docker create --name "$PIPELINE_NAME" \
               -e PIPELINE="$PIPELINE_NAME" \
@@ -51,15 +48,12 @@ docker create --name "$PIPELINE_NAME" \
               -e AZURE_TENANT_ID="$AZURE_TENANT_ID" \
               bureaugewas/vgdedatasets:latest
 
-# Step 4: Set up cron job on Raspberry Pi
-echo "Step 4: Setting up cron job"
-(crontab -l 2>/dev/null; echo "00 02 * * * PIPELINE_NAME=$PIPELINE_NAME && \
-docker rm \$PIPELINE_NAME && \
-docker start \$PIPELINE_NAME >> /var/log/myjob.log 2>&1") | crontab -
-
 # Clean up: Remove the cloned repository
 echo "Cleaning up: Removing cloned repository"
 cd ..
 rm -rf vgdedatasets_deploy
 
 echo "Deployment script completed for pipeline: $PIPELINE_NAME."
+
+# Manualy add it to "crontab -e":
+# 00 02 * * * PIPELINE_NAME=pipeline_test && docker start $PIPELINE_NAME ; docker stop $PIPELINE_NAME
