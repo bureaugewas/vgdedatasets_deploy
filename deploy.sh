635762bd0a557238226141b0e916701f9350690e
jurjenwerkaccount <jurjen.v.genugten@gmail.com>
2023-11-28 19:40:10 +0100

    Different approach

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # Check if all required arguments are provided
-if [ "$#" -ne 7 ]; then
-  echo "Usage: $0 <pipeline_name> <github_token> <docker_username> <docker_password> <azure_client_id> <azure_client_secret> <azure_tenant_id>"
+if [ "$#" -ne 8 ]; then
+  echo "Usage: $0 <pipeline_name> <host> <password> <github_token> <docker_username> <docker_password>"
   exit 1
 fi
 
 PIPELINE_NAME="$1"
-GITHUB_TOKEN="$2"
-DOCKER_USERNAME="$3"
-DOCKER_PASSWORD="$4"
-AZURE_CLIENT_ID="$5"
-AZURE_CLIENT_SECRET="$6"
-AZURE_TENANT_ID="$7"
+HOST="$2"
+PASSWORD="$3"
+GITHUB_TOKEN="$4"
+DOCKER_USERNAME="$5"
+DOCKER_PASSWORD="$6"
 
 # Clone the GitHub repository
 echo "Cloning repository: bureaugewas/vgde_datasets_pipeline"
@@ -21,35 +20,28 @@ git clone https://$GITHUB_TOKEN@github.com/bureaugewas/vgde_datasets_pipeline vg
 # Step into the cloned repository
 cd vgdedatasets_deploy || exit 1
 
-# Stop and remove the existing Docker container
-echo "Stopping and removing existing Docker container: $PIPELINE_NAME"
-docker stop "$PIPELINE_NAME" || true
-docker rm "$PIPELINE_NAME" || true
-
 # Remove the existing Docker image
 echo "Removing existing Docker image: bureaugewas/vgdedatasets"
 docker rmi bureaugewas/vgdedatasets:latest || true
 
-# Step 1: Build Docker image
-echo "Step 1: Building Docker image"
-export DOCKER_CONFIG=$HOME/.docker
-mkdir -p $DOCKER_CONFIG
-echo "$DOCKER_PASSWORD" | sudo docker login --username "$DOCKER_USERNAME" --password-stdin
+# Step 1: Build Docker image locally
+echo "Step 1: Building Docker image locally"
 docker build --build-arg GITHUB_TOKEN=$GITHUB_TOKEN -t vgdedatasets .
 
 # Step 2: Push Docker image to Docker Hub
 echo "Step 2: Pushing Docker image to Docker Hub"
+docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 docker tag vgdedatasets bureaugewas/vgdedatasets:latest
 docker push bureaugewas/vgdedatasets:latest
 
-# Step 3: Create Docker container on Raspberry Pi (azure vars should be set in the machine's environment)
+# Step 3: Connect to Raspberry Pi via SSH and create Docker container
 echo "Step 3: Creating Docker container on Raspberry Pi"
-docker create --name "$PIPELINE_NAME" \
-              -e PIPELINE="$PIPELINE_NAME" \
-              -e AZURE_CLIENT_ID="$AZURE_CLIENT_ID" \
-              -e AZURE_CLIENT_SECRET="$AZURE_CLIENT_SECRET" \
-              -e AZURE_TENANT_ID="$AZURE_TENANT_ID" \
-              bureaugewas/vgdedatasets:latest
+sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no $HOST "docker pull bureaugewas/vgdedatasets:latest && \
+                        docker stop $PIPELINE_NAME || true && \
+                        docker rm $PIPELINE_NAME || true && \
+                        docker create --name $PIPELINE_NAME \
+                                      -e PIPELINE=$PIPELINE_NAME \
+                                      bureaugewas/vgdedatasets:latest"
 
 # Clean up: Remove the cloned repository
 echo "Cleaning up: Removing cloned repository"
@@ -57,6 +49,3 @@ cd ..
 rm -rf vgdedatasets_deploy
 
 echo "Deployment script completed for pipeline: $PIPELINE_NAME."
-
-# Manualy add it to "crontab -e":
-# 00 02 * * * PIPELINE_NAME=pipeline_test && docker start $PIPELINE_NAME ; docker stop $PIPELINE_NAME
